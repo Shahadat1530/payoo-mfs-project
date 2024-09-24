2e7fc832385972027826d7d7e19e2f9056ccbe0d
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2024-09-24 12:35:45 +0600

    redireacted to new html

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,14 @@
 document.getElementById('btn-login').addEventListener('click', function(event){
     event.preventDefault();
+
     const phoneNumber = document.getElementById('phn-num').value;
-    console.log(phoneNumber);
+    const pinNumer = document.getElementById('pin-num').value;
+    
+    if (phoneNumber === '017' && pinNumer === '1234') {
+        console.log('logged in!!');
+        window.location.href = '/home.html';
+    }
+    else {
+        alert('Wrong phone or pin!!!');
+    }
 })
